10aa0bbc0f91e6be0b50643ea28367bb4aaaa8bd
Naoya Yamashita <conao3@gmail.com>
2023-06-27 18:36:04 +0900

    create subr folder

--- src/html_lisfy/subr/itertools.py
+++ src/html_lisfy/subr/itertools.py
@@ -1,19 +1,10 @@
-from __future__ import annotations
-
+from typing import Iterable, Callable
 import typing
-from typing import Iterable
-from collections.abc import Callable
 
 import more_itertools
 
 
 T = typing.TypeVar('T')
-def trap(fn: Callable[[], T]) -> tuple[T, None] | tuple[None, Exception]:
-    try:
-        return fn(), None
-    except Exception as e:
-        return None, e
-
 
 def takewhile_inclusive(pred: Callable[[T], bool], peekable: more_itertools.peekable[T]) -> Iterable[T]:
     while True:
